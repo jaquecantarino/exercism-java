6ce744027497c7d02cfbf2d0f9e3dea854b4b071
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2022-05-03 21:53:51 -0300

    exericicos iniciais

---
+++ CookYourLasagna/src/CozinheSuaLasanha.java
@@ -0,0 +1,17 @@
+
+//public class CozinheSuaLasanha {
+
+//	public static void main(String[] args) {
+//		
+//	}
+
+//	public CozinheSuaLasanha() {
+//		// TODO Auto-generated constructor stub
+//	}
+	
+//	public int expectedMinutesInOven() {
+//		return 40; 
+//	}
+	
+	
+
